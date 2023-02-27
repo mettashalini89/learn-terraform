5aa997dda502894380c9eaeb8248380c80016113
mettashalini89@gmail.com <CatMatDog@123>
2023-02-27 12:17:08 -0700

    output creation

---
+++ output/output.tf
@@ -0,0 +1,9 @@
+output "sample"{
+  value = "Hello World"
+}
+
+output "sample1"{
+  value = "hello all"
+}
+
+#every thing is a block in terraform, we can have multiple outputs in a file and output is to print the given
