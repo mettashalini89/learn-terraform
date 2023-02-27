8c8f174f591315c3459bb48a3abad7fdf2612647
mettashalini89@gmail.com <CatMatDog@123>
2023-02-27 11:30:53 -0700

    resource creation

--- 01.sample/test.tf
+++ 01.sample/test.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "ec2" {
   ami = "ami-0a017d8ceb274537d"
   instance_type = "t3.micro"
-  vpc_security_group_ids = [sg-043fd9cc9443cbb75]
+  vpc_security_group_ids = ["sg-043fd9cc9443cbb75"]
   tags = {
     Name = "test"
   }
