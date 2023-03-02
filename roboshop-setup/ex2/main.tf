b5bdf91b8b32fbf2c20854c38a3b93db9a8b8e22
mettashalini89@gmail.com <CatMatDog@123>
2023-03-01 19:38:29 -0700

    using count as a loop

---
+++ roboshop-setup/ex2/main.tf
@@ -0,0 +1,12 @@
+data "aws_ami" "ami" {
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+  owners           = ["973714476881"]
+}
+
+resource "aws_instance" "ec2" {
+  count = 5 #count used to iterate the resources, just a loop
+  ami = "ami-0a017d8ceb274537d"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-043fd9cc9443cbb75"]
+}
