bf0a4df5b10d86f27a4638d4b3e181b2b3119611
mettashalini89@gmail.com <CatMatDog@123>
2023-03-02 12:12:58 -0700

    apply modules to create roboshop instances itterate module

---
+++ roboshop-setup/ex3/ec2/main.tf
@@ -0,0 +1,25 @@
+data "aws_ami" "ami" {
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+  owners           = ["973714476881"]
+}
+
+resource "aws_instance" "ec2"{  #first lable is from terraform and second lable is any can be given by user for his ref
+  ami = "ami-0a017d8ceb274537d"
+  instance_type = var.instance_type
+  vpc_security_group_ids = ["sg-043fd9cc9443cbb75"]
+  tags = {
+    Name = var.component
+  }
+}
+
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z02543141QBSJJNNYEJL"
+  name    = "${var.component}-dev.devopsb71.live"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.ec2.private_ip]
+}
+
+variable "component" {}
+variable "instance_type" {}
