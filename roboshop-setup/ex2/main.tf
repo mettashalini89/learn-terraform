1cc8e6cc5b565888d30e866979cdabe7d17ddc58
mettashalini89@gmail.com <CatMatDog@123>
2023-03-01 19:50:30 -0700

    using count as a loop

--- roboshop-setup/ex2/main.tf
+++ roboshop-setup/ex2/main.tf
@@ -5,8 +5,18 @@ data "aws_ami" "ami" {
 }
 
 resource "aws_instance" "ec2" {
-  count = 5 #count used to iterate the resources, just a loop
+#  count = 5 #count used to iterate the resources, just a loop
+  count = length(var.instances)
   ami = "ami-0a017d8ceb274537d"
   instance_type = "t3.micro"
   vpc_security_group_ids = ["sg-043fd9cc9443cbb75"]
+}
+
+variable "instances" {
+  default = ["cart", "frontend", "catalogue", "user", "shipping"]
+}
+
+output "public_ips"{
+  value = aws_instance.ec2.*.public_ip #star is used for all publicips , for particular we need to use index no
+
 }
