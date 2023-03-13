089ff9e047060ec572f6fb1e5e08bfbb9aac4934
mettashalini89@gmail.com <CatMatDog@123>
2023-03-13 12:14:57 -0600

    adding env variable

--- prometheus-test-server/main.tf
+++ prometheus-test-server/main.tf
@@ -9,6 +9,12 @@ resource "aws_spot_instance_request" "node1" {
   }
 }
 
+resource "aws_ec2_tag" "node1" {
+  resource_id = aws_spot_instance_request.node1.spot_instance_id
+  key         = "Name"
+  value       = "test server"
+}
+
 resource "aws_spot_instance_request" "node2" {
   ami           = "ami-0a017d8ceb274537d"
   instance_type = "t3.micro"
@@ -18,4 +24,10 @@ resource "aws_spot_instance_request" "node2" {
   tags = {
     Name = "test-node"
   }
+}
+
+resource "aws_ec2_tag" "node2" {
+  resource_id = aws_spot_instance_request.node2.spot_instance_id
+  key         = "Name"
+  value       = "test node"
 }
