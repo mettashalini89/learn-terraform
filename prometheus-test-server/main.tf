c734c1c7414dc8d389eb511e2d4bdc6ba3d3509f
mettashalini89@gmail.com <CatMatDog@123>
2023-03-13 12:02:54 -0600

    adding env variable

---
+++ prometheus-test-server/main.tf
@@ -0,0 +1,21 @@
+resource "aws_spot_instance_request" "node1" {
+  ami           = "ami-0a017d8ceb274537d"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [sg-043fd9cc9443cbb75]
+  wait_for_fulfillment = true
+
+  tags = {
+    Name = "test-server"
+  }
+}
+
+resource "aws_spot_instance_request" "node2" {
+  ami           = "ami-0a017d8ceb274537d"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [sg-043fd9cc9443cbb75]
+  wait_for_fulfillment = true
+
+  tags = {
+    Name = "test-node"
+  }
+}
